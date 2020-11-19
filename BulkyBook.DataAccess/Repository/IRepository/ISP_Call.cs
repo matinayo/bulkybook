d6f073ea1643c01bff7696401ab64ea7bad19630
abdul-matin0 <madeayo04@gmail.com>
2020-11-19 22:20:11 +0100

    Add project files.

---
+++ BulkyBook.DataAccess/Repository/IRepository/ISP_Call.cs
@@ -0,0 +1,25 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    public interface ISP_Call : IDisposable
+    {
+        // returns single value
+        T Single<T> (string procedureName, DynamicParameters param = null);
+
+        // execute to a database and not retrieving anything
+        void Execute(string procedureName, DynamicParameters param = null);
+
+        // returns one complete record
+        T OneRecord<T>(string procedureName, DynamicParameters param = null);
+
+        // returns all rows
+        IEnumerable<T> List<T>(string procedureName, DynamicParameters param = null);
+
+        // returns two tables
+        Tuple<IEnumerable<T1>, IEnumerable<T2>> List<T1, T2>(string procedureName, DynamicParameters param = null);
+    }
+}
---
+++ BulkyBook/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BulkyBook.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll() {
+            // retrieve all categories and return in json format
+            var allObj = _unitOfWork.Category.GetAll();
+
+            return Json(new { data = allObj });
+        }
+
+        #endregion
+    }
+}
