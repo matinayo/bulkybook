bdfb363e12179a057ddcb2891d507d129fd35158
abdul-matin0 <madeayo04@gmail.com>
2020-12-04 15:53:42 +0100

    Order Management

---
+++ BulkyBook.Models/ViewModel/OrderDetailsVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulkyBook.Models.ViewModel
+{
+    public class OrderDetailsVM
+    {
+        public OrderHeader OrderHeader { get; set; }
+        public IEnumerable<OrderDetails> OrderDetails { get; set; }
+    }
+}
